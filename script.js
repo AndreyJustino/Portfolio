9cf3427b28cc328b96af23f2c89d481384dfa7bb
AndreyJustino <andreyjustino2020@gmail.com>
2024-08-26 15:49:07 -0300

    rodapé da pagina

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-const btn_abrir = document.getElementById("btn-abrir")
+const btn_abrir = document.getElementById("btn-abrir");
 
 btn_abrir.addEventListener("click", () => {
-  
-    const menu = document.getElementById("menu-mobile")
-    const overlay_menu = document.getElementById("overlay-menu")
+  const menu = document.getElementById("menu-mobile");
+  const overlay_menu = document.getElementById("overlay-menu");
 
-    if(menu.style.display = "none"){
-        menu.style.display = "block"
-        overlay_menu.style.display = "block"
-        btn_abrir.style.display = "none"
-    }
-
+  if ((menu.style.display = "none")) {
+    menu.style.display = "block";
+    overlay_menu.style.display = "block";
+    btn_abrir.style.display = "none";
+  }
 });
 
-const btn_fechar = document.getElementById("btn-fechar")
+const btn_fechar = document.getElementById("btn-fechar");
 
 btn_fechar.addEventListener("click", () => {
-    const menu = document.getElementById("menu-mobile")
-    const overlay_menu = document.getElementById("overlay-menu")
-    menu.style.display = "none"
-    overlay_menu.style.display = "none"
-    btn_abrir.style.display = "block"
+  const menu = document.getElementById("menu-mobile");
+  const overlay_menu = document.getElementById("overlay-menu");
+  menu.style.display = "none";
+  overlay_menu.style.display = "none";
+  btn_abrir.style.display = "block";
+});
 
-})
+function rta() {
+  let tamanho = document.documentElement.scrollWidth;
+  if (tamanho >= 796) {
+    document.getElementById("overlay-menu").style.display = "none";
+    document.getElementById("btn-abrir").style.display = "none";
+  }
 
-function rta(){
-    let tamanho = document.documentElement.scrollWidth;
-    if(tamanho >= 796){
-        document.getElementById("overlay-menu").style.display = "none"
-        document.getElementById("btn-abrir").style.display = "none"
+  if (tamanho <= 795) {
+    document.getElementById("menu-mobile").style.display = "none";
+    document.getElementById("btn-abrir").style.display = "block";
+
+    if (document.getElementById("btn-abrir").style.display == "block") {
+      document.getElementById("overlay-menu").style.display = "none";
     }
-
-    if(tamanho <= 795){
-        document.getElementById("menu-mobile").style.display = "none"
-        document.getElementById("btn-abrir").style.display = "block"
-        
-        if(document.getElementById("btn-abrir").style.display == "block"){
-            document.getElementById("overlay-menu").style.display = "none"
-        }
-    }
+  }
 }
